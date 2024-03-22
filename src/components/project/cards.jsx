8dae3072e664ff3207e12fddbfd945885165222d
Shivam-21-11 <shivamsingh2111@gmail.com>
2024-03-22 17:26:55 +0530

    Slider SM Fix1

--- src/components/project/cards.jsx
+++ src/components/project/cards.jsx
@@ -18,7 +18,7 @@ const Cards = ({project}) => {
         <div className="text-center text-sm text-gray-500 pt-5">Tools: {tools.join(', ')}</div>
     </motion.div>
 
-  <motion.div onClick={handleClick} whileTap={{scale:0.9 , borderColor: "#FFFFFF", borderWidth: "2px" }} transition={{duration:0.2}} className="glass-beige shadow-md rounded-md p-4 items-center w-[22em] h-[20em] block sm:hidden">
+  <motion.div onClick={handleClick} whileTap={{scale:0.9 , borderColor: "#FFFFFF", borderWidth: "2px" }} transition={{duration:0.2}} className="glass-beige shadow-md rounded-md p-4 items-center w-[20em] h-[20em] block sm:hidden">
     <div className="mb-10 mt-6 flex justify-center"><img src={icon} alt="Logo" style={{filter:'brightness(0) invert(1)'}}/></div>
     <div className="text-center mb-4 text-white bold">{name}</div> 
     <div className="text-center mb-2 text-gray-400 light">{truncatedSummary}</div>
--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -18,7 +18,7 @@ function Project({apiLink}){
       infinite: true,
       speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 1,
+      // slidesToScroll: 1,
       responsive: [
         {
           breakpoint: 1024,
